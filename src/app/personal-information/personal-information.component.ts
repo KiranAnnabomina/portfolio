1b49501c25e860b6ca2e5a10d86dd51665ba4c37
Kiran Annabomina <Kiran@PRADEEPs-MacBook-Air-2.local>
2023-03-08 02:19:23 -0800

    Personal Information component is completed.

--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'personal-information',
   templateUrl: './personal-information.component.html',
   styleUrls: ['./personal-information.component.css']
 })
-export class PersonalInformationComponent {
+export class PersonalInformationComponent implements OnInit {  
+  myData: string[][] = [
+    ['Name', 'Kiran Annabomina'],
+    ['DOB', '05/05/1992'],
+    ['Work Exp', '8 Years'],
+    ['Education', 'M.Sc(2014)'],
+    ['Interests', 'Gaming'],
+  ];
+
+  aboutMe: string[] = [
+    'Passionate front-end web developer with 8+ years of experience.',
+    'Worked on JavaScript, HTML5, and CSS to build all aspects of the user experience and user interface for client-facing landing pages.',
+    'Specializes in using AngularJS plus modern libraries and frameworks to build e-commerce sites.'
+  ];
+
+  constructor() { }
+  
+  ngOnInit(): void { }
 
 }
